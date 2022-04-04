16eb6fbe9ac90cb4867f9b02b8693d863230fba1
DanyGuerra <luis_daniel.grra15@hotmail.com>
2022-04-04 18:37:25 -0500

    Fecha y hora de registro y respuesta con pagina renderizada

--- index.js
+++ index.js
@@ -27,6 +27,21 @@ app.post("/", async (req, res) => {
   const correo = req.body.correo;
   const titulo = req.body.titulo;
 
+  let fecha = new Date();
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  fecha.toLocaleDateString("default", options);
+
+  let fecharegistro =
+    fecha.toLocaleDateString("default", options) +
+    " " +
+    fecha.toLocaleTimeString("default");
+
   if (!nombre && !correo && !titulo) {
     res.sendStatus(400);
   }
@@ -52,21 +67,21 @@ app.post("/", async (req, res) => {
   try {
     await googleSheetsInstance.spreadsheets.values.append({
       auth, //auth object
-      spreadsheetId, //spreadsheet id
+      spreadsheetId: "asdf", //spreadsheet id
       range: "A:C", //sheet name and range of cells
       valueInputOption: "USER_ENTERED", // The information will be passed according to what the usere passes in as date, number or text
       resource: {
-        values: [[nombre, correo, titulo]],
+        values: [[nombre, correo, titulo, fecharegistro]],
       },
     });
 
-    res.sendStatus(200);
+    res.render("exito", { titulo: "My page" }).sendStatus(200);
   } catch (error) {
-    res.sendStatus(500);
+    res.render("fallo", { titulo: "My page" }).sendStatus(500);
     console.error(error);
   }
 });
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log(`Example app listening on http://localhost:4000`);
+  console.log(`Example app listening on http://localhost:5000`);
 });
