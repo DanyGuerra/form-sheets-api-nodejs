5d8349223742591e28167633606226f43b88ff1d
DanyGuerra <luis_daniel.grra15@hotmail.com>
2022-04-21 16:21:03 -0500

    Agregando catcha

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ app.post("/", async (req, res) => {
   const correo = req.body.correo;
   const titulo = req.body.titulo;
 
+  if (!nombre || !correo || !titulo) {
+    return;
+  }
+
   let fecha = new Date();
   const options = {
     weekday: "short",
